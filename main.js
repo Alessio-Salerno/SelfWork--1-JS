33ebd1ba036df0d6b2d6b71e40b79791d61776e9
Alessio-Salerno <156314493+Alessio-Salerno@users.noreply.github.com>
2024-05-17 15:57:59 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+let num1 = 5;
+console.log(num1);
+
+
+let num2 = 6;
+console.log(num2);
+
+let stringa = 'Ciao a tutti';
+console.log(stringa);
+stringa = 'Ciaone!'
+console.log(stringa);
+
+
+const PIGRECO = 3.14159265359
+console.log(PIGRECO);
+PIGRECO = 3.250819;
+console.log(PIGRECO);
